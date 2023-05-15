4eab5c71bd694dd5afd630c6e446a4cd6c6a0923
Brezzor <oliv095w@edu.zealand.dk>
2023-05-15 14:27:53 +0200

    Changed router again

--- src/router/router.js
+++ src/router/router.js
@@ -30,7 +30,7 @@ const router = createRouter({
       component: () => import('../views/UserUVView.vue')
     },
     {
-      path: '/:pathMatch(.*)*',
+      path: '/:catchAll(.*)*',
       name: 'notFound', 
       redirect: '/' 
     }
