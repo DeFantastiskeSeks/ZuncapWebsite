432c0def09c47c5759bd6f25a282316b8956dec0
Simon Søby <sb.simonzone@gmail.com>
2023-05-16 14:32:24 +0200

    Now logs in automaticly Co-creator: Ruben

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,51 +1,43 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   data() {
     return {
       userName: null,
       password: null,
-      cExpiryDays: 30
+      cExpiryDays: 30,
     };
   },
   methods: {
-    SetCookie: function(cname, cvalue, cexpiredays){
-      //SetCookie: function(cname, cvalue, cPassword, cPasswordValue, cexpiredays){
+    SetCookie: function (cname, cvalue, cPasswordValue, cexpiredays) {
       const expirationDate = new Date();
-      expirationDate.setDate((expirationDate.getDate)+(cexpiredays*24*60*60*1000));
+      expirationDate.setDate(
+        expirationDate.getDate + cexpiredays * 24 * 60 * 60 * 1000
+      );
       let expires = "expires= " + expirationDate.toUTCString();
-      document.cookie = cname + "=" + cvalue + ";"  + expires + ";path=/";
-      //document.cookie = cname + "=" + cvalue + ";" + cPassword + "=" + cPasswordValue + ";"  + expires + ";path=/";
+      document.cookie =
+        cname + "=" + cvalue + "," + cPasswordValue + ";" + expires + ";path=/";
     },
-    GetCookie: function(cname){
-      let name = cname + "=";
-      let decodedCookie = decodeURIComponent(document.cookie);
-      let ca = decodedCookie.split(';');
-      for (let i = 0; i<ca.length; i++){
-        let c = ca[i];
-        while (c.charAt(0)== ''){
-          c= c.substring(1);
-        }
-        if (c.indexOf(name)==0){
-          return c.substring(name.length, c.length);
+    GetCookie: function (name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie !== "") {
+        var cookies = document.cookie.split(";");
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          if (cookie.substring(0, name.length + 1) === name + "=") {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            if (cookieValue.indexOf(",") !== -1) {
+              cookieValue = cookieValue.split(",");
+            }
+            break;
+          }
         }
       }
-      return "";
-    },
-    CheckCookie: function(){
-      let username = this.GetCookie("userName");
-      if (username != ""){
-        alert("Hej igen" + username);
-      } else {
-        username = prompt("Indtast venligst brugernavn:", "");
-        if (username != "" && username != null){
-          this.SetCookie("userName", username, this.cExpiryDays)
-        }
-      }
+      return cookieValue;
     },
     async PostLogin() {
-      axios.post("https://zuncapapi.azurewebsites.net/api/Users/login", 
-        {
+      axios
+        .post("https://zuncapapi.azurewebsites.net/api/Users/login", {
           name: this.userName,
           password: this.password,
         })
@@ -54,22 +46,32 @@ export default {
         })
         .catch(function (error) {
           console.log(error);
-        })
-        console.log(this.userName + ' ' + this.password);
-        //this.SetCookie("userName", this.userName, "password", this.password, this.cExpiryDays)
-        this.SetCookie("userName", this.userName, this.cExpiryDays)
-
+        });
+      console.log(this.userName + " " + this.password);
+      this.SetCookie(
+        "userName",
+        this.userName,
+        this.password,
+        this.cExpiryDays
+      );
     },
   },
-  mounted: function() {
-    console.log("mounting")
-    let cookie = this.GetCookie("userName")
-    console.log(cookie)
-    console.log("mounted")
-  }
-  
+  mounted: function () {
+    console.log("mounting");
+    console.log(this.userName + " " + this.password);
+    let cookie = this.GetCookie("userName", "password");
+    if (cookie != null) {
+      this.userName = cookie[0];
+      this.password = cookie[1];
+      if (this.userName && this.password != null) {
+        this.PostLogin();
+      }
+    }
+    console.log(this.userName + " " + this.password);
+    console.log("mounted");
+  },
 };
-</script> 
+</script>
 
 <template>
   <div class="container">
@@ -84,7 +86,8 @@ export default {
                 v-model="this.userName"
                 type="text"
                 class="form-control"
-                placeholder="your name" />
+                placeholder="Brugernavn"
+              />
             </div>
             <div class="form-group">
               <label for="Password">Password</label>
@@ -92,18 +95,16 @@ export default {
                 v-model="this.password"
                 type="password"
                 class="form-control"
-                placeholder="Your Password" />
-            </div>
-            <div class="form-check">
-              <input
-                type="checkbox"
-                class="form-check-input"
+                placeholder="Password"
               />
-              <label class="form-check-label"
-                >Husk mig</label
-              >
             </div>
-            <button type="submit" class="btn btn-primary" name="submitbtn">Login</button>
+            <div hidden class="form-check">
+              <input type="checkbox" class="form-check-input" />
+              <label class="form-check-label">Husk mig</label>
+            </div>
+            <button type="submit" class="btn btn-primary" name="submitbtn">
+              Login
+            </button>
           </div>
         </form>
       </div>
